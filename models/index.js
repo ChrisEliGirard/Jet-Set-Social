c6dd66409f86b03c0271cc4e0fef00c752e49969
ikubiti <ikubiti@icloud.com>
2023-02-23 21:03:38 -0500

    Defined the associations between the models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,104 @@
-const User = require('./User');
-const Trip = require('./Trip');
+const Users = require('./Users');
+const Trips = require('./Trips');
+const Tagged = require('./Tagged');
+const Images = require('./Images');
+const Locations = require('./Locations');
+const Comments = require('./Comments');
 
-User.hasMany(Trips, {
+// A user can have many trips
+Users.hasMany(Trips, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Trip.belongsTo(User, {
+// A trip belongs to one user
+Trips.belongsTo(Users, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+// A user can have many images
+Users.hasMany(Images, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+// Images belong to one user
+Images.belongsTo(Users, {
+  foreignKey: 'user_id'
+});
+
+// A user can belong to many tagged
+Users.hasMany(Tagged, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+// Tagged belongs to one user
+Tagged.belongsTo(Users, {
+  foreignKey: 'user_id'
+});
+
+// A user can have many comments
+Users.hasMany(Comments, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+// Comments belongs to one user
+Comments.belongsTo(Users, {
+  foreignKey: 'user_id'
+});
+
+// Trips have many Tagged
+Trips.hasMany(Tagged, {
+  foreignKey: 'trip_id',
+  onDelete: 'CASCADE'
+});
+
+// Tagged belongs to one trip
+Tagged.belongsTo(Trips, {
+  foreignKey: 'trip_id'
+});
+
+// A trip can have many comments
+Trips.hasMany(Comments, {
+  foreignKey: 'trip_id',
+  onDelete: 'CASCADE'
+});
+
+// Comments belongs to one trip
+Comments.belongsTo(Trips, {
+  foreignKey: 'trip_id'
+});
+
+// A location can have many Comments
+Locations.hasMany(Comments, {
+  foreignKey: 'location_id',
+  onDelete: 'CASCADE'
+});
+
+// Comments belongs to one location
+Comments.belongsTo(Locations, {
+  foreignKey: 'location_id'
+});
+
+// Tagged has a many to many relations with Comments through Trips
+Tagged.belongsToMany(Comments, {
+  through: {
+    model: Trips,
+    unique: false
+  },
+  as: 'TaggedUsers'
+});
+
+// Comments has a many to many relations with Tagged through Trips
+Comments.belongsToMany(Tagged, {
+  through: {
+    model: Trips,
+    unique: false
+  },
+  as: 'TaggedComments',
+});
+
+
+module.exports = { Users, Trips, Tagged, Images, Locations, Comments };
