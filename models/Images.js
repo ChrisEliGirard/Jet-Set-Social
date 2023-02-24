def0d88427b7b68f5451d64c6b5d1b26005ed7fb
ikubiti <ikubiti@icloud.com>
2023-02-23 19:57:48 -0500

    Defined the models for the application

---
+++ models/Images.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Images extends Model { }
+
+Images.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    image: {
+      type: DataTypes.BLOB('long'),
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+    },
+    trip_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'trips',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'trips',
+  }
+);
+
+module.exports = Images;
