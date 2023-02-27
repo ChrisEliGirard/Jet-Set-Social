d7e2b4091d2680cfe3aaebe16aace1944b342e5e
Chris Girard <chrisegirard@gmail.com>
2023-02-27 18:43:54 -0500

    added a semicolon but im switching branches so stashing change

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const tripRoutes = require('./tripRoutes');
 const commentRoutes = require('./commentRoutes');
-const locationRoutes = require('./locationRoutes')
+const locationRoutes = require('./locationRoutes');
 
 router.use('/users', userRoutes);
 router.use('/trips', tripRoutes);
