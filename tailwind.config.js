cd066b669fb792a309af8004433ac0c574c0c859
Chris Girard <chrisegirard@gmail.com>
2023-03-02 22:41:29 -0500

    TailwindsCss file changes and links to get style sheet working

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.handlebars", "./views/*.handlebars", "./public/js/*.js"],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
