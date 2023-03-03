9e96f56b173491151f3441f26543d36d45edb611
Chris Girard <chrisegirard@gmail.com>
2023-03-02 23:37:50 -0500

    s

--- public/js/search.js
+++ public/js/search.js
@@ -1,12 +1,15 @@
-const searchQuery = async (event) => {
+const searchQuery = (event) => {
     event.preventDefault();
 
     //Collect Search Inquiry from the searchbar
-    const searchRaw = document.querySelector('.search-bar').value.trim().toLowerCase();
-    document.location.replace(`/search/${searchRaw}`);
+    const searchRaw = document.querySelector('.userInput').value.trim().toLowerCase();
+     document.location.replace('/search/'+searchRaw);
 };
 
 
+const init = () => {
+    const searchEl = document.getElementById('.userInput');
+    searchEl.addEventListener('input', searchQuery);
+};
 
-
-document.querySelector('.search-bar').addEventListener('submit', searchQuery);
+window.onload = init();
