604cc48d7b160aded4f64db2bc001d5707980df5
Chris Girard <chrisegirard@gmail.com>
2023-03-05 20:03:49 -0500

    search view and search.js is working now showing user and trip results

--- public/js/search.js
+++ public/js/search.js
@@ -3,11 +3,12 @@ const searchQuery = async (event) => {
 
     //Collect Search Inquiry from the searchbar
     const searchRaw = document.querySelector('#search-bar').value.trim().toLowerCase();
+    console.log(searchRaw)
     document.location.replace('/search/'+searchRaw);
 };
 
 
 
 
-const searchEl = document.querySelector('#search-bar');
+const searchEl = document.querySelector('#search-form');
 searchEl.addEventListener('submit', searchQuery);
