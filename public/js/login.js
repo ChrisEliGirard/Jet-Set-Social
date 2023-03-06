3ceb8b81e3739aa3e11eb84b14c26cb19b0ef7e2
ikubiti <ikubiti@icloud.com>
2023-03-05 21:50:37 -0500

    implemented new user signup and profile picture rendering

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,5 @@
+
+
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
@@ -22,55 +24,7 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const name = document.querySelector('#name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  if (name && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
 
 document
   .querySelector('#login-form')
   .addEventListener('click', loginFormHandler);
-
-// document
-//   .querySelector('#signup-form')
-//   .addEventListener('submit', signupFormHandler);
-
-
-// const sign_in_btn = document.querySelector("#sign-in-btn");
-// const sign_up_btn = document.querySelector("#sign-up-btn");
-// const container = document.querySelector(".container");
-// const sign_in_btn2 = document.querySelector("#sign-in-btn2");
-// const sign_up_btn2 = document.querySelector("#sign-up-btn2");
-
-// sign_up_btn.addEventListener("click", () => {
-//   container.classList.add("sign-up-mode");
-// });
-
-// sign_in_btn.addEventListener("click", () => {
-//   container.classList.remove("sign-up-mode");
-// });
-
-// sign_up_btn2.addEventListener("click", () => {
-//   container.classList.add("sign-up-mode2");
-// });
-
-// sign_in_btn2.addEventListener("click", () => {
-//   container.classList.remove("sign-up-mode2");
-// });
